da9f326f1832c3de91eca8c7924d7358a084e7a9
Jakub Tolsciuk <jakubtolsciuk@gmail.com>
2022-02-08 23:49:13 +0100

    Add loading bar for upload and add axios

--- frontend/src/components/ImageUploader.js
+++ frontend/src/components/ImageUploader.js
@@ -2,18 +2,34 @@
 import {
 	Box,
 	Button,
+	TextField,
+	Typography,
+	LinearProgress,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogTitle
 } from '@mui/material';
 
 // * images
 import backgroundImage from '../images/footer_lodyas.png';
 
+// * requests
+import axios from 'axios';
+
 import React, { useState } from "react";
 import ImageUploading from 'react-images-uploading';
 import "../css/General.css";
 
+import {Link} from 'react-router-dom';
+import { useLocation } from 'react-router-dom'
+
 export default function ImageUploader(props) {
 
 	const [uploadedImages, setUploadedImages] = useState([]);
+	const [uploadProgress, setUploadProgress] = useState(0);
+	const [isUploadDialogOpen, setIsUploadDialogOpen] = useState(false);
+	const location = useLocation();
 
 	const onUploadChange = (imageList, addUpdateIndex) => {
 		// data for submit
@@ -21,151 +37,213 @@ export default function ImageUploader(props) {
 		setUploadedImages(imageList);
 	};
 
-	// TODO add token bearer
 	const uploadImages = async () => {
+
+		setIsUploadDialogOpen(true);
 
 		const formData = new FormData();
 		for (var i = 0; i < uploadedImages.length; i++) formData.append('new_pictures', uploadedImages[i].file);
 
-		try {
-			const response = await fetch(
-				`/api/images/upload/${props.year}/${props.location}`,
-				{
-					method: "POST",
-					headers: {
-						Authorization: `Bearer ${props.tokenValue}`,
-					},
-					body: formData
-				}
-			);
-			console.log(response);
-
-		} catch (error) {
-			console.error("Error: ", error);
-		}
+		axios.request({
+			method: "post",
+			url: `/api/images/upload/${props.year}/${props.location}`,
+			headers: {
+				Authorization: `Bearer ${props.tokenValue}`,
+			},
+			data: formData,
+			onUploadProgress: (p) => {
+				var progress = parseFloat((p.loaded / p.total).toFixed(2));
+				console.log(progress);
+				setUploadProgress(progress);
+			}
+		}).then(data => {
+			console.log("All loaded - 100%");
+			setUploadProgress(1.00);
+		})
 	};
 
+	const handleUploadDialogExit = () => {
+    setIsUploadDialogOpen(false);
+    // TODO handle errors and text fields
+  };
+
+	function LinearProgressWithLabel(props) {
+		return (
+			<Box sx={{ display: 'flex', alignItems: 'center' }}>
+				<Box sx={{ width: '100%', mr: 1 }}>
+					<LinearProgress variant="determinate" {...props} />
+				</Box>
+				<Box sx={{ minWidth: 35 }}>
+					<Typography variant="body2" color="text.secondary">{`${Math.round(
+						props.value,
+					)}%`}</Typography>
+				</Box>
+			</Box>
+		);
+	}
+
 	return (
-		<Box
-			sx={{
-				width: "900px",
-				height: "700px",
-				border: "solid",
-				borderRadius: "5%",
-				backgroundImage: `url(${backgroundImage})`,
-			}}
-			className="center"
-		>
-			<Box>
-				<ImageUploading
-					multiple
-					value={uploadedImages}
-					onChange={onUploadChange}
-					maxNumber={69}
-					dataURLKey="data_url"
-				>
-					{({
-						imageList,
-						onImageUpload,
-						onImageRemoveAll,
-						onImageUpdate,
-						onImageRemove,
-						isDragging,
-						dragProps,
-					}) => (
-						// * write your building UI
-						<Box
-							sx={{
-								// backgroundColor: "gray",
-							}}
-						>
-							<Button
-								sx={{
-									margin: "5px"
-								}}
-								style={isDragging ? { color: 'red' } : undefined}
-								onClick={onImageUpload}
-								{...dragProps}
-								type="submit"
-								size="large"
-								color="secondary"
-								variant="contained"
-							>
-								Click or Drop here
-							</Button>
-
-							<Button
-								sx={{
-									margin: "5px"
-								}}
-								type="submit"
-								size="large"
-								color="secondary"
-								variant="contained"
-								onClick={onImageRemoveAll}
-							>
-								Remove all images
-							</Button>
-
+		<>
+			<Box
+				sx={{
+					width: "900px",
+					height: "700px",
+					border: "solid",
+					borderRadius: "5%",
+					backgroundImage: `url(${backgroundImage})`,
+				}}
+				className="center"
+			>
+				<Box>
+					<ImageUploading
+						multiple
+						value={uploadedImages}
+						onChange={onUploadChange}
+						maxNumber={69}
+						dataURLKey="data_url"
+					>
+						{({
+							imageList,
+							onImageUpload,
+							onImageRemoveAll,
+							onImageUpdate,
+							onImageRemove,
+							isDragging,
+							dragProps,
+						}) => (
+							// * write your building UI
 							<Box
 								sx={{
-									height: "580px",
 									// backgroundColor: "gray",
-									display: "flex",
-									flexDirection: "row",
-									flexWrap: "wrap",
-									alignContent: "flex-start",
-									overflowY: 'auto', // display scroll bar after overflow
-									paddingLeft: "15px"
 								}}
 							>
-								{imageList.map((image, index) => (
-									<Box
-										sx ={{
-											width: "100px",
-											height: "100px",
-											margin: "3px"
-										}}
-										key={index}
-									>
-										<span style={{ display: "inline-block", height: "100%", verticalAlign: "middle" }} />
-										<img 
-											style={{
-												height: "auto",
-												width: "auto",
-												maxWidth: "100px",
-												maxHeight: "100px", 
-												borderRadius: "5%",
-												verticalAlign: "middle"
+								<Button
+									sx={{
+										margin: "5px"
+									}}
+									style={isDragging ? { color: 'red' } : undefined}
+									onClick={onImageUpload}
+									{...dragProps}
+									type="submit"
+									size="large"
+									color="secondary"
+									variant="contained"
+								>
+									Click or Drop here
+								</Button>
+
+								<Button
+									sx={{
+										margin: "5px"
+									}}
+									type="submit"
+									size="large"
+									color="secondary"
+									variant="contained"
+									onClick={onImageRemoveAll}
+								>
+									Remove all images
+								</Button>
+
+								<Box
+									sx={{
+										height: "580px",
+										// backgroundColor: "gray",
+										display: "flex",
+										flexDirection: "row",
+										flexWrap: "wrap",
+										alignContent: "flex-start",
+										overflowY: 'auto', // display scroll bar after overflow
+										paddingLeft: "15px"
+									}}
+								>
+									{imageList.map((image, index) => (
+										<Box
+											sx={{
+												width: "100px",
+												height: "100px",
+												margin: "3px"
 											}}
-											src={image['data_url']}
-											alt=""
-										/>
-										{/* Additional buttons for each picutre if needed */}
-										{/* <div className="image-item__btn-wrapper">
-											<button onClick={() => onImageUpdate(index)}>Update</button>
-											<button onClick={() => onImageRemove(index)}>Remove</button>
-										</div> */}
-									</Box>
-								))}
+											key={index}
+										>
+											<span style={{ display: "inline-block", height: "100%", verticalAlign: "middle" }} />
+											<img
+												style={{
+													height: "auto",
+													width: "auto",
+													maxWidth: "100px",
+													maxHeight: "100px",
+													borderRadius: "5%",
+													verticalAlign: "middle"
+												}}
+												src={image['data_url']}
+												alt=""
+											/>
+										</Box>
+									))}
+								</Box>
 							</Box>
-						</Box>
-					)}
-				</ImageUploading>
+						)}
+					</ImageUploading>
+				</Box>
+
+				<Button
+					sx={{
+						marginTop: "10px",
+					}}
+					type="submit"
+					size="large"
+					color="secondary"
+					variant="contained"
+					onClick={uploadImages}
+				>
+					Upload images
+				</Button>
 			</Box>
 
-			<Button
-				sx={{
-					marginTop: "10px",
-				}}
-				type="submit"
-				size="large"
-				color="secondary"
-				variant="contained"
-				onClick={uploadImages}
+			{/* // Dialog window */}
+			<Dialog
+				open={isUploadDialogOpen}
+				onClose={handleUploadDialogExit}
+				aria-labelledby="form-dialog-title"
 			>
-				Upload images
-			</Button>
-		</Box>
+				<DialogTitle id="form-dialog-title">
+					<div>
+						<div>
+							<Typography variant="h6">
+								Upload progress
+							</Typography>
+						</div>
+					</div>
+				</DialogTitle>
+
+				<DialogContent
+					sx={{
+						width: "300px"
+					}}
+				>
+					<Box sx={{ width: '100%' }}>
+						<LinearProgressWithLabel value={uploadProgress*100} />
+					</Box>
+				</DialogContent>
+
+				<DialogActions>
+					{/* Redirect to the same page to see uplaoded pictures */}
+					<Link to={location.pathname} className="temp">
+						{/* reload(true) */}
+						<Button 
+							onClick={() => {
+								handleUploadDialogExit();
+								window.location.reload(true);
+							}}
+							color="primary"
+						>
+							Ok
+						</Button>
+					</Link>
+				</DialogActions>
+
+			</Dialog>
+		</>
 	);
 }
--- frontend/src/pages/ChooseLocation.js
+++ frontend/src/pages/ChooseLocation.js
@@ -1,14 +1,14 @@
-import {  
+import {
   Button,
   Typography,
   TextField,
   Dialog,
-  DialogActions, 
+  DialogActions,
   DialogContent,
   DialogTitle
 }
-from '@mui/material';
-import {Link} from 'react-router-dom';
+  from '@mui/material';
+import { Link } from 'react-router-dom';
 import React, { useEffect, useState, useContext } from "react";
 import { AppContext } from './../contexts/AppContext';
 import "../css/ChooseYearLocation.css";
@@ -19,91 +19,91 @@ import useStyles from '../styles/ChooseYearLocationStyles';
 
 export default function ChooseLocation() {
 
-    const classes = useStyles();
-    const {year} = useParams();
-    const [availableLocations, setAvailableLocations] = useState([]);
-    const [isAddLocationDialogOpen, setIsAddLocationDialogOpen] = useState(false);
-    const [newLocationToAdd, setNewLocationToAdd] = useState([]);
-    const {setBackNavPage, tokenValue} = useContext(AppContext);
-    
+  const classes = useStyles();
+  const { year } = useParams();
+  const [availableLocations, setAvailableLocations] = useState([]);
+  const [isAddLocationDialogOpen, setIsAddLocationDialogOpen] = useState(false);
+  const [newLocationToAdd, setNewLocationToAdd] = useState([]);
+  const { setBackNavPage, tokenValue } = useContext(AppContext);
 
-    const fetchLocations = async () => {
-        try {
-          const response = await fetch(
-            `/api/locations/${year}`,
-            {
-              method: "GET",
-              headers: {
-                "Content-Type": "application/json",
-              },
-            }
-          )
-          const dataLocations = await response.json();
-          setAvailableLocations(dataLocations);
-          console.log(dataLocations);
-        } catch (error) {
-          console.error("Error: ", error);
+
+  const fetchLocations = async () => {
+    try {
+      const response = await fetch(
+        `/api/locations/${year}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-      };
+      )
+      const dataLocations = await response.json();
+      setAvailableLocations(dataLocations);
+      console.log(dataLocations);
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+  };
 
-      const addNewLocation = async () => {
-        try {
-          const response = await fetch(
-            `/api/locations/create/location/${year}/${newLocationToAdd}`,
-            {
-              method: "POST",
-              headers: {
-                Authorization: `Bearer ${tokenValue}`,
-                "Content-Type": "application/json",
-              },
-            }
-          )
-          const res = await response.json();
-          fetchLocations();
-          console.log(res);
-        } catch (error) {
-          console.error("Error: ", error);
+  const addNewLocation = async () => {
+    try {
+      const response = await fetch(
+        `/api/locations/create/location/${year}/${newLocationToAdd}`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${tokenValue}`,
+            "Content-Type": "application/json",
+          },
         }
-        handleLocationDialogExit();
-      };
+      )
+      const res = await response.json();
+      fetchLocations();
+      console.log(res);
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+    handleLocationDialogExit();
+  };
 
-      const handleLocationDialogExit = () => {
-        setIsAddLocationDialogOpen(false);
-        // TODO handle errors and text fields
-      };
+  const handleLocationDialogExit = () => {
+    setIsAddLocationDialogOpen(false);
+    // TODO handle errors and text fields
+  };
 
-      useEffect(() => {
-        setBackNavPage('/years');
-        fetchLocations();   //
-      }, []);
+  useEffect(() => {
+    setBackNavPage('/years');
+    fetchLocations();   //
+  }, []);
 
 
-    return (
-      <>
-        <div id="yearsLocationsContainer" className="center">
-          {availableLocations.map((location) => {
-              return <Link to={`/${year}/${location}`} className=""> <div className="year-location-box" key={location}>{location}</div> </Link>
-          })
-          
-          }
-          {
-              tokenValue && 
-              
-                <div className="year-location-box" key="addYear" onClick={() => {setIsAddLocationDialogOpen(true)}}>
-                  <AddIcon
-                    fontSize="large"
-                    className={classes.addIcon}
-                  />
-                </div> 
-              
-            }
-          <div style={{clear: "both"}}></div>
-        </div>
-        <Dialog
-          open={isAddLocationDialogOpen}
-          onClose={handleLocationDialogExit}
-          aria-labelledby="form-dialog-title"
-        >
+  return (
+    <>
+      <div id="yearsLocationsContainer" className="center">
+        {availableLocations.map((location) => {
+          return <Link to={`/${year}/${location}`} className=""> <div className="year-location-box" key={location}>{location}</div> </Link>
+        })
+
+        }
+        {
+          tokenValue &&
+
+          <div className="year-location-box" key="addYear" onClick={() => { setIsAddLocationDialogOpen(true) }}>
+            <AddIcon
+              fontSize="large"
+              className={classes.addIcon}
+            />
+          </div>
+
+        }
+        <div style={{ clear: "both" }}></div>
+      </div>
+      <Dialog
+        open={isAddLocationDialogOpen}
+        onClose={handleLocationDialogExit}
+        aria-labelledby="form-dialog-title"
+      >
         <DialogTitle id="form-dialog-title">
           <div>
             <div>
@@ -134,13 +134,13 @@ export default function ChooseLocation() {
             </Button>
           </div>
 
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleLocationDialogExit} color="primary">
-              Close
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </>
-    );
-  }
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleLocationDialogExit} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
