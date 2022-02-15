a2af73cfff4fd5b1e8027ce6e3f388ddcc821e89
Jakub Tolsciuk <jakubtolsciuk@gmail.com>
2022-02-15 17:07:36 +0100

    Add login feedback

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,11 +1,17 @@
-import { Button, TextField } from '@mui/material';
+// Material UI
+import {
+  Box,
+  Button,
+  TextField,
+  Typography
+} from '@mui/material';
+
+// * images
+import backgroundImage from '../images/footer_lodyas.png';
+
 import React, { useState, useContext } from "react";
 import { AppContext } from './../contexts/AppContext';
 import "../css/General.css";
-import "../css/Login.css";
-import { ThemeProvider } from '@mui/styles';
-import formTheme from "../themes/formTheme";
-import useStyles from '../styles/LoginStyles';
 import { useHistory } from "react-router-dom";
 
 
@@ -13,8 +19,7 @@ export default function Login() {
     const {setTokenCookie, setTokenValue} = useContext(AppContext);
     const [username, setUsername] = useState();
     const [password, setPassword] = useState();
-    const [loginError, setLoginError] = useState(false);  //TODO GUI info about error
-    const classes = useStyles();
+    const [loginError, setLoginError] = useState();
     const history = useHistory();
 
     const handleLoginButton = async() => {
@@ -37,44 +42,84 @@ export default function Login() {
             setTokenCookie('token', data.access_token);   //set cookie
             history.push('/');      // redirect to home page
             setLoginError(false);
-          } else setLoginError(true);
+          } else setLoginError("Wrong username or password!");
         })
         .catch((error) => {
           console.error(error);
-          setLoginError(true);
+          setLoginError("Wrong username or password!");
         });
     };
 
+
     return (
-    <ThemeProvider theme={formTheme}>
-      <div 
-        id="loginInputContainer"
+      <Box
+        sx={{
+          backgroundImage: `url(${backgroundImage})`,
+          width: "400px",
+          height: "340px",
+          borderRadius: "5%",
+          borderStyle: "solid",
+          borderColor: "white",
+        }}
       >
-          <h2>Sign In</h2>
+          <Typography
+            sx={{
+              marginTop: "20px",
+              marginBottom: "20px"
+            }}
+            variant='h5'>Sign In</Typography>
+          {/* <h2>Sign In</h2> */}
+
           <TextField
+            sx={{
+              color: 'black',
+              backgroundColor: 'white',
+              marginBottom: "30px",
+            }}
             color="primary"
             variant="filled"
             label="Username"
             placeholder="Enter username"
             type="username"
             fullWidth
-            className={classes.loginInput}
             onChange={(event) => {
               setUsername(event.target.value);
             }}
           />
+
           <TextField
+            sx={{
+              color: 'black',
+              backgroundColor: 'white',
+              marginBottom: "15px",
+            }}
             color="primary"
             label="Password"
             variant="filled"
             placeholder="Enter password"
             type="password"
             fullWidth
-            className={classes.loginInput}
             onChange={(event) => {
               setPassword(event.target.value);
             }}
           />
+
+          {/* Error message */}
+          <Box
+            sx={{
+              color: 'red',
+              lineHeight: '30px',
+              fontWeight: "bold",
+              width: "400px",
+              height: "30px",
+              marginBottom: "15px",
+              textAlign: 'center',
+              // backgroundColor: 'yellow'
+            }}
+          >
+            {loginError}
+          </Box>
+
           <Button
             type="submit"
             size="large"
@@ -84,7 +129,6 @@ export default function Login() {
           >
             Sign in
           </Button>
-      </div>
-    </ThemeProvider>
+      </Box>
     );
   }
