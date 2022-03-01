c51a7b0f08d5150fd689539527b7765ed2618ff8
Jakub Tolsciuk <jakubtolsciuk@gmail.com>
2022-03-01 23:13:20 +0100

    Add pylint setup

--- backend/src/routers/categories.py
+++ backend/src/routers/categories.py
@@ -1,11 +1,10 @@
 """
-{'category': 'cars', 'year': 2016, 'location': 'Greece', 'image_name': 'smth.jpg'}
-Landscape, Cars, Flora, Birds, Wildlife 
+    TODO module docstring
 """
 from tinydb import TinyDB, where, Query
 from fastapi import APIRouter, Depends
-from src.routers.auth import get_current_user
 from fastapi.responses import FileResponse
+from src.routers.auth import get_current_user
 
 
 CATEGORIES_DB_PATH = "./data/DB/categories.json"
@@ -14,17 +13,30 @@ CATEGORIES_DB = TinyDB(CATEGORIES_DB_PATH)
 router = APIRouter(prefix="/api/categories", tags=["Categories"])
 
 @router.post("/add/{category}/{year}/{location}/{image_name}")
-async def add_image_to_category(category : str, year : int, location : str, image_name : str, user: dict = Depends(get_current_user)):
-    categories_query = CATEGORIES_DB.search(Query().fragment({'category': category, 'year': year, 'location': location, 'image_name': image_name}))
+async def add_image_to_category(category : str, year : int,
+ location : str, image_name : str, user: dict = Depends(get_current_user)):
+    """
+        TODO docstring
+    """
+    categories_query = CATEGORIES_DB.search(Query().fragment(
+     {'category': category, 'year': year, 'location': location, 'image_name': image_name}))
     if categories_query:
         return f"Picture {image_name} already exists in this category"
-    CATEGORIES_DB.insert({'category': category, 'year': year, 'location': location, 'image_name': image_name})
-    return f"Added new picture {image_name} to {category} category by {user['username']}"   # TODO change for response
+    CATEGORIES_DB.insert({'category': category, 'year': year,
+     'location': location, 'image_name': image_name})
+    return f"""Added new picture {image_name} to {category}
+     category by {user['username']}"""   # TODO change for response
 
 @router.post("/delete/{category}/{year}/{location}/{image_name}")
-async def delete_image_from_category(category : str, year : int, location : str, image_name : str, user: dict = Depends(get_current_user)):
-    CATEGORIES_DB.remove(Query().fragment({'category': category, 'year': year, 'location': location, 'image_name': image_name}))
-    return f"Removed picture {image_name} from {category} category by {user['username']}"   # TODO change for response
+async def delete_image_from_category(category : str, year : int,
+ location : str, image_name : str, user: dict = Depends(get_current_user)):
+    """
+        TODO docstring
+    """
+    CATEGORIES_DB.remove(Query().fragment({'category': category,
+     'year': year, 'location': location, 'image_name': image_name}))
+    return f"""Removed picture {image_name} from {category}
+     category by {user['username']}"""   # TODO change for response
 
 @router.get("/get/{year}/{location}/{image_name}")
 async def get_image_categories(year : int, location : str, image_name : str):
@@ -32,22 +44,32 @@ async def get_image_categories(year : int, location : str, image_name : str):
         TODO docstring
         Get choosen categories for a specific image.
     """
-    landscape_query = CATEGORIES_DB.search(Query().fragment({'category': 'landscape', 'year': year, 'location': location, 'image_name': image_name}))
-    cars_query = CATEGORIES_DB.search(Query().fragment({'category': 'cars', 'year': year, 'location': location, 'image_name': image_name}))
-    flora_query = CATEGORIES_DB.search(Query().fragment({'category': 'flora', 'year': year, 'location': location, 'image_name': image_name}))
-    birds_query = CATEGORIES_DB.search(Query().fragment({'category': 'birds', 'year': year, 'location': location, 'image_name': image_name}))
-    wildlife_query = CATEGORIES_DB.search(Query().fragment({'category': 'wildlife', 'year': year, 'location': location, 'image_name': image_name}))
-    return {'landscape': landscape_query != [], 'cars': cars_query != [], 'flora': flora_query != [], 'birds': birds_query != [], 'wildlife': wildlife_query != []}
+    landscape_query = CATEGORIES_DB.search(Query().fragment({'category': 'landscape',
+     'year': year, 'location': location, 'image_name': image_name}))
+    cars_query = CATEGORIES_DB.search(Query().fragment({'category': 'cars',
+     'year': year, 'location': location, 'image_name': image_name}))
+    flora_query = CATEGORIES_DB.search(Query().fragment({'category': 'flora',
+     'year': year, 'location': location, 'image_name': image_name}))
+    birds_query = CATEGORIES_DB.search(Query().fragment({'category': 'birds',
+     'year': year, 'location': location, 'image_name': image_name}))
+    wildlife_query = CATEGORIES_DB.search(Query().fragment({'category': 'wildlife',
+     'year': year, 'location': location, 'image_name': image_name}))
+    return {'landscape': landscape_query != [], 'cars': cars_query != [],
+     'flora': flora_query != [], 'birds': birds_query != [], 'wildlife': wildlife_query != []}
 
 
 
 @router.get("/content/{category}/{img_id}")
 async def get_image_from_category(category : str, img_id : int):
+    """
+        TODO docstring
+    """
     categories_query = CATEGORIES_DB.search(where('category') == category)
     if not categories_query:
-        return f"There is no image"
+        return "There is no image"
     image_data = categories_query[img_id-1]
-    image_content = FileResponse(f"./data/images/{image_data['year']}/{image_data['location']}/{image_data['image_name']}")
+    image_content = FileResponse(f"""./data/images/{image_data['year']}
+    /{image_data['location']}/{image_data['image_name']}""")
     return image_content
 
 @router.get("/names/{category}")
@@ -58,7 +80,7 @@ async def get_images_names_from_category(category : str):
     """
     categories_query = CATEGORIES_DB.search(where('category') == category)
     if not categories_query:
-        return f"There are no images in this category"
+        return "There are no images in this category"
     image_names = [image_data["image_name"] for image_data in categories_query]
     return {"img_names": image_names}
 
@@ -69,7 +91,8 @@ async def get_thumbnail_from_category(category : str, img_id : int):
     """
     categories_query = CATEGORIES_DB.search(where('category') == category)
     if not categories_query:
-        return f"There are no images in this category"
+        return "There are no images in this category"
     image_data = categories_query[img_id-1]
-    image_content = FileResponse(f"./data/imagesThumbs/{image_data['year']}/{image_data['location']}/{image_data['image_name']}")
+    image_content = FileResponse(f"""./data/imagesThumbs/{image_data['year']}
+     /{image_data['location']}/{image_data['image_name']}""")
     return image_content
--- backend/src/routers/images.py
+++ backend/src/routers/images.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, File, UploadFile, Depends
-from fastapi.responses import FileResponse
+"""
+    TODO module docstring
+"""
 import os
 from typing import List
-# from PIL import Image
 import shutil
+from fastapi import APIRouter, File, UploadFile, Depends
+from fastapi.responses import FileResponse
 from src.utils.thumbnailsGenerator import create_thumbnails
 from src.utils.general import create_dir_if_not_exists
 from src.routers.auth import get_current_user
@@ -12,6 +14,9 @@ router = APIRouter(prefix="/api/images", tags=["Images"])
 
 @router.get("/content/{year}/{location}/{img_id}")
 async def get_image_content(year : str, location : str, img_id : int):
+    """
+    TODO docstring
+    """
     path = f"./data/images/{year}/{location}"
     try:
         all_images = os.listdir(path)   # returns list of files (names)
@@ -32,6 +37,9 @@ async def get_image_content(year : str, location : str, img_id : int):
 
 @router.get("/names/{year}/{location}")
 async def get_images_names(year : str, location : str):
+    """
+    TODO docstring
+    """
     path = f"./data/images/{year}/{location}"
     try:
         all_images = os.listdir(path)   # returns list of files (names)
@@ -40,12 +48,12 @@ async def get_images_names(year : str, location : str):
         return {"img_names": []}
 
     # img_id: 1, 2, 3, 4, ...
-    all_images = [img for img in all_images]  # img.split('.')[0]
+    all_images = list(all_images)
     return {"img_names": all_images}
 
 @router.get("/thumbnails/{year}/{location}")
 async def get_images_thumbnails(year : str, location : str):
-    """ 
+    """
     TODO send it with one request
     https://stackoverflow.com/questions/61163024/return-multiple-files-from-fastapi
     """
@@ -57,12 +65,15 @@ async def get_images_thumbnails(year : str, location : str):
 
     # img_id: 1, 2, 3, 4, ...
     # ! this is wrong, it should be zipped
-    all_thumbs = [FileResponse(f"./data/imagesThumbs/{year}/{location}/{thumb}") for thumb in all_thumbs]
+    all_thumbs = [FileResponse(f"./data/imagesThumbs/{year}/{location}/{thumb}")
+     for thumb in all_thumbs]
     return all_thumbs
-    # return {"all_thumbs": all_thumbs}
 
 @router.get("/thumbnail/{year}/{location}/{img_id}")
-async def get_images_thumbnails(year : str, location : str, img_id : int):
+async def get_image_thumbnail(year : str, location : str, img_id : int):
+    """
+    TODO docstring
+    """
     path = f"./data/imagesThumbs/{year}/{location}"
     try:
         all_thumbnails = os.listdir(path)   # returns list of files (names)
@@ -74,10 +85,11 @@ async def get_images_thumbnails(year : str, location : str, img_id : int):
     thumbnail_content = FileResponse(f"./data/imagesThumbs/{year}/{location}/{thumbnail_name}")
     return thumbnail_content
 
-@router.post("/upload/{year}/{location}") 
-async def upload_images(year : str, location : str, new_pictures: List [UploadFile] = File(...), user: dict = Depends(get_current_user)): 
+@router.post("/upload/{year}/{location}")
+async def upload_images(year : str, location : str,
+ new_pictures: List [UploadFile] = File(...), user: dict = Depends(get_current_user)):
     """
-    probably not needed anymore
+    TODO docstring
     """
     path = f"./data/images/{year}/{location}"
     create_dir_if_not_exists(path)
@@ -88,4 +100,3 @@ async def upload_images(year : str, location : str, new_pictures: List [UploadFi
     create_thumbnails(path)
 
     return f"Added new photos by {user['username']}"
-
