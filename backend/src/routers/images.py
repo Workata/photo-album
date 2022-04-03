edce1cc4af76753c87c3044c6b0a7d8dd35cb137
Jakub Tolsciuk <jakubtolsciuk@gmail.com>
2022-04-03 23:01:34 +0200

    Add images order

--- backend/src/routers/images.py
+++ backend/src/routers/images.py
@@ -3,6 +3,7 @@
 """
 import os
 from typing import List
+from tinydb import TinyDB, where, Query
 from fastapi import APIRouter, File, UploadFile, Depends
 from fastapi.responses import FileResponse
 from PIL import Image
@@ -10,28 +11,27 @@ from src.utils.thumbnailsGenerator import create_thumbnails
 from src.utils.general import create_dir_if_not_exists
 from src.routers.auth import get_current_user
 
+
 router = APIRouter(prefix="/api/images", tags=["Images"])
+
+IMAGES_ORDER_DB_PATH = "./data/DB/imagesOrder.json"
+IMAGES_ORDER_DB = TinyDB(IMAGES_ORDER_DB_PATH)
 
 @router.get("/content/{year}/{location}/{img_id}")
 async def get_image_content(year : str, location : str, img_id : int):
     """
     TODO docstring
     """
-    path = f"./data/images/{year}/{location}"
-    try:
-        all_images = os.listdir(path)   # returns list of files (names)
-    except:
+    images_order_query = IMAGES_ORDER_DB.search(Query().fragment(
+     {'year': year, 'location': location}))
+    if not images_order_query:
         print("This folder doesn't exist (probably)")
         return None
 
-    # * img id valdiation
-    if img_id > len(all_images)  or img_id < 1:
-        print("index out of range")
-        # set default value
-        img_id = 1
+    images_order = images_order_query[0]['order']
 
-    # img_id: 1, 2, 3, 4, ...
-    image_name = all_images[img_id-1]
+    # img_id: <1, N>
+    image_name = images_order[str(img_id)]
     image_content = FileResponse(f"./data/images/{year}/{location}/{image_name}")
     return image_content
 
@@ -40,48 +40,34 @@ async def get_images_names(year : str, location : str):
     """
     TODO docstring
     """
-    path = f"./data/images/{year}/{location}"
-    try:
-        all_images = os.listdir(path)   # returns list of files (names)
-    except:
+    images_order_query = IMAGES_ORDER_DB.search(Query().fragment(
+     {'year': year, 'location': location}))
+    if not images_order_query:
         print("This folder doesn't exist (probably)")
         return {"img_names": []}
+    images_names = []
+    images_order = images_order_query[0]['order']
 
-    # img_id: 1, 2, 3, 4, ...
-    all_images = list(all_images)
-    return {"img_names": all_images}
+    for i in range(1, len(images_order) + 1):
+        images_names.append(images_order[str(i)])
 
-@router.get("/thumbnails/{year}/{location}")
-async def get_images_thumbnails(year : str, location : str):
-    """
-    TODO send it with one request
-    https://stackoverflow.com/questions/61163024/return-multiple-files-from-fastapi
-    """
-    path = f"./data/imagesThumbs/{year}/{location}"
-    try:
-        all_thumbs = os.listdir(path)   # returns list of files (names)
-    except:
-        print("This folder doesn't exist (probably)")
-
-    # img_id: 1, 2, 3, 4, ...
-    # ! this is wrong, it should be zipped
-    all_thumbs = [FileResponse(f"./data/imagesThumbs/{year}/{location}/{thumb}")
-     for thumb in all_thumbs]
-    return all_thumbs
+    return {"img_names": images_names}
 
 @router.get("/thumbnail/{year}/{location}/{img_id}")
 async def get_image_thumbnail(year : str, location : str, img_id : int):
     """
     TODO docstring
     """
-    path = f"./data/imagesThumbs/{year}/{location}"
-    try:
-        all_thumbnails = os.listdir(path)   # returns list of files (names)
-    except:
+    images_order_query = IMAGES_ORDER_DB.search(Query().fragment(
+     {'year': year, 'location': location}))
+    if not images_order_query:
         print("This folder doesn't exist (probably)")
+        return None
 
-    # img_id: 1, 2, 3, 4, ...
-    thumbnail_name = all_thumbnails[img_id-1]
+    images_order = images_order_query[0]['order']
+
+    # * img_id: <1, N>
+    thumbnail_name = images_order[str(img_id)]
     thumbnail_content = FileResponse(f"./data/imagesThumbs/{year}/{location}/{thumbnail_name}")
     return thumbnail_content
 
@@ -91,9 +77,14 @@ def upload_images(year : str, location : str,
     """
     TODO docstring
     """
-    # print(f"'new_pictures' (List) {new_pictures}")
-    for image in new_pictures:
+    images_order = {}
+
+    for idx, image in enumerate(new_pictures):
+        images_order[idx + 1] = image.filename
         print(image.filename)   # this is ok
+
+    IMAGES_ORDER_DB.insert({'year': year,
+     'location': location, 'order': images_order})
 
     path = f"./data/images/{year}/{location}"
     create_dir_if_not_exists(path)
